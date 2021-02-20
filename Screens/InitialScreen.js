65e8650ef8cd50e70f8267319c302ef7451ae97b
jamesossam <JOssam@scu.edu>
2021-02-20 16:34:37 -0500

    Implemented Redux, and gave small sample

--- Screens/InitialScreen.js
+++ Screens/InitialScreen.js
@@ -1,24 +1,40 @@
 import React, { Component } from 'react';
 import { View, TouchableOpacity, Text, StyleSheet } from 'react-native';
 
+import { connect } from 'react-redux';
+import { setUserType } from "../actions";
+
 class InitialScreen extends Component {
     constructor() {
         super();
+        this.setUserType = this.setUserType.bind(this);
+    }
 
+    setUserType(userType) {
+        if (userType === "Donor") {
+            this.props.dispatch(setUserType("Donor"));
+        }
+        else if (userType === "Recipient") {
+            this.props.dispatch(setUserType("Recipient"));
+        }
+        else {
+            console.error(`Invalid user type pushed: userType = ${userType}`);
+        }
     }
 
     render() {
         return (
             <View style={styles.wrapper}>
-                <TouchableOpacity style={styles.donorButton}>
+                <TouchableOpacity style={styles.donorButton} onPress={() => this.setUserType("Donor")}>
                     <Text style={styles.textStyles}>Donor</Text>
                 </TouchableOpacity>
-                <TouchableOpacity style={styles.receiverButton}>
-                    <Text style={styles.textStyles}>Receiver</Text>
+                <TouchableOpacity style={styles.receiverButton} onPress={() => this.setUserType("Recipient")}>
+                    <Text style={styles.textStyles}>Recipient</Text>
                 </TouchableOpacity>
             </View>
         )
     }
+
 }
 
 const styles = StyleSheet.create({
@@ -54,4 +70,7 @@ const styles = StyleSheet.create({
     }
 });
 
-export default InitialScreen;
+const mapStateToProps = state => ({
+    userType: state.userType,
+})
+export default connect(mapStateToProps)(InitialScreen);

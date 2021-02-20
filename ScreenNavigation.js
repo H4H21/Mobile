1e6e44fcb1023e801a6719c5fcf43b9a1cdc7018
jamesossam <JOssam@scu.edu>
2021-02-20 18:36:24 -0500

    Navigation between screens now works

--- ScreenNavigation.js
+++ ScreenNavigation.js
@@ -4,6 +4,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import InitialScreen from "./Screens/InitialScreen";
+import DonorScreen from "./Screens/DonorScreen";
+import RecipientScreen from "./Screens/RecipientScreen";
 
 function ScreenNavigation(props) {
     const stack = createStackNavigator();
@@ -15,6 +17,8 @@ function ScreenNavigation(props) {
         <NavigationContainer>
             <stack.Navigator initialRouteName="Initial" screenOptions={{headerShown: false}} >
                 <stack.Screen name="Initial" component={InitialScreen} />
+                <stack.Screen name="DonorScreen" component={DonorScreen}/>
+                <stack.Screen name="RecipientScreen" component={RecipientScreen}/>
             </stack.Navigator>
         </NavigationContainer>
     );

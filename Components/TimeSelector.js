1642591b6dd6b67df22711f483cb8b29677a365f
jamesossam <JOssam@scu.edu>
2021-02-21 02:11:01 -0500

    Made changes to TimeSelector to make it easier to get the time out of it

--- Components/TimeSelector.js
+++ Components/TimeSelector.js
@@ -1,54 +1,106 @@
-import * as React from 'react'
-import { Button } from 'react-native-paper'
-import { TimePickerModal } from 'react-native-paper-dates'
+import React, { Component } from 'react';
+import { Button } from 'react-native-paper';
+import { TimePickerModal } from 'react-native-paper-dates';
+import { View, StyleSheet, TouchableOpacity, Text } from 'react-native';
+import PropTypes from 'prop-types';
 
-export default function TimeSelector() {
-  var today = new Date();
-  var hour;
-  const [visible, setVisible] = React.useState(false)
-  const [StartPressed, setStartPressed] = React.useState(false)
-  const [EndPressed, setEndPressed] = React.useState(false)
+class TimeSelector extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            date: null,
+            hours: null,
+            minutes: null,
+            visible: false,
+        }
+        this.now = new Date();
+        this.hours = this.now.getHours();
+        this.minutes = this.now.getMinutes();
+        this.onDismiss = this.onDismiss.bind(this);
+        this.onConfirm = this.onConfirm.bind(this);
+    }
 
-  if (StartPressed) {
-    hour = today.getHours();
-  }
-  else if (EndPressed) {
-    hour = today.getHours() + 1;
-  }
+    onDismiss() {
+        this.setState({visible: false});
+    }
 
-  const onDismiss = React.useCallback(() => {
-    setVisible(false)
-  }, [setVisible])
+    onConfirm(params) {
+        console.dir(params);
+        this.setState({hours: params.hours, minutes: params.minutes, visible: false});
+        //YYYY-MM-DDTHH:MM:SSZ
+        let hours = params.hours;
+        let minutes = params.minutes;
+        console.log(`hours: ${hours}, minutes: ${minutes}`);
+        if (hours < 10) {
+            if (minutes < 10) {
+                this.props.getTimeCallback(`2021-02-21T0${hours}:0${minutes}:00Z`);
+            } else {
+                // hours < 10, minutes >= 10
+                this.props.getTimeCallback(`2021-02-21T0${hours}:${minutes}:00Z`);
+            }
+        } else {
+            if (minutes < 10) {
+                this.props.getTimeCallback(`2021-02-21T${hours}:0${minutes}:00Z`);
+            } else {
+                this.props.getTimeCallback(`2021-02-21T${hours}:${minutes}:00Z`);
+            }
+        }
+    }
 
-  const onConfirm = React.useCallback(
-    ({ hours, minutes }) => {
-      setVisible(false);
-      console.log({ hours, minutes });
-    },
-    [setVisible]
-  );
+    render() {
+        return (
+            <View style={styles.wrapper}>
+                <TimePickerModal
+                    visible={this.state.visible}
+                    onDismiss={this.onDismiss}
+                    onConfirm={this.onConfirm}
+                    hours={this.hours} // default: current hours
+                    minutes={this.minutes} // default: current minutes
+                    // am_pm = "pm" // default: 'pm'
+                    label="Select time" // optional, default 'Select time'
+                    cancelLabel="Cancel" // optional, default: 'Cancel'
+                    confirmLabel="Ok" // optional, default: 'Ok'
+                    animationType="fade" // optional, default is 'none'
+                    locale={'en'} // optional, default is automically detected by your system
+                />
 
-  return (
-    <>
-      <TimePickerModal
-        visible={visible}
-        onDismiss={onDismiss}
-        onConfirm={onConfirm}
-        hours={hour} // default: current hours
-        minutes={today.getMinutes()} // default: current minutes
-        // am_pm = "pm" // default: 'pm'
-        label="Select time" // optional, default 'Select time'
-        cancelLabel="Cancel" // optional, default: 'Cancel'
-        confirmLabel="Ok" // optional, default: 'Ok'
-        animationType="fade" // optional, default is 'none'
-        locale={'en'} // optional, default is automically detected by your system
-      />
-      <Button onPress={() => { setVisible(true), setStartPressed(true), setEndPressed(false)}}>
-        Start Time
-      </Button>
-      <Button onPress={() => { setVisible(true), setEndPressed(true), setStartPressed(false)}}>
-        End Time
-      </Button>
-    </>
-  )
+                <TouchableOpacity style={styles.button} onPress={() => {this.setState({visible: true})}}>
+                    <Text style={styles.buttonText}>{this.props.buttonText}</Text>
+                </TouchableOpacity>
+            </View>
+        );
+    }
+
+
 }
+
+TimeSelector.propTypes = {
+    getTimeCallback: PropTypes.func.isRequired,
+    buttonText: PropTypes.string.isRequired,
+};
+
+const styles = StyleSheet.create({
+    wrapper: {
+        marginTop: '5%',
+        width: '90%',
+        height: '20%',
+
+    },
+
+    button: {
+        height: '100%',
+        width: '90%',
+        backgroundColor: 'blue',
+        borderRadius: 20,
+        justifyContent: 'center',
+    },
+
+    buttonText: {
+        textAlign: 'center',
+        fontSize: 20,
+        color: 'white',
+    }
+
+});
+
+export default TimeSelector;

7f40cfa4e13eebf23e4755c5f5d1720157795693
George Kouretas <thegreekgeorge07@gmail.com>
2021-02-20 17:06:32 -0800

    Updated components

--- Components/TimeSelector.js
+++ Components/TimeSelector.js
@@ -3,19 +3,30 @@ import { Button } from 'react-native-paper'
 import { TimePickerModal } from 'react-native-paper-dates'
 
 export default function TimeSelector() {
+  var today = new Date();
+  var hour;
   const [visible, setVisible] = React.useState(false)
+  const [StartPressed, setStartPressed] = React.useState(false)
+  const [EndPressed, setEndPressed] = React.useState(false)
+
+  if (StartPressed) {
+    hour = today.getHours();
+  }
+  else if (EndPressed) {
+    hour = today.getHours() + 1;
+  }
+
   const onDismiss = React.useCallback(() => {
     setVisible(false)
   }, [setVisible])
 
   const onConfirm = React.useCallback(
-    ({ hours, minutes, am_pm }) => {
+    ({ hours, minutes }) => {
       setVisible(false);
-      console.log({ hours, minutes, am_pm });
+      console.log({ hours, minutes });
     },
     [setVisible]
   );
-
 
   return (
     <>
@@ -23,17 +34,20 @@ export default function TimeSelector() {
         visible={visible}
         onDismiss={onDismiss}
         onConfirm={onConfirm}
-        hours={12} // default: current hours
-        minutes={14} // default: current minutes
-        am_pm = "pm" // default: 'pm'
+        hours={hour} // default: current hours
+        minutes={today.getMinutes()} // default: current minutes
+        // am_pm = "pm" // default: 'pm'
         label="Select time" // optional, default 'Select time'
         cancelLabel="Cancel" // optional, default: 'Cancel'
         confirmLabel="Ok" // optional, default: 'Ok'
         animationType="fade" // optional, default is 'none'
         locale={'en'} // optional, default is automically detected by your system
       />
-      <Button onPress={()=> setVisible(true)}>
-        Enter available time
+      <Button onPress={() => { setVisible(true), setStartPressed(true), setEndPressed(false)}}>
+        Start Time
+      </Button>
+      <Button onPress={() => { setVisible(true), setEndPressed(true), setStartPressed(false)}}>
+        End Time
       </Button>
     </>
   )

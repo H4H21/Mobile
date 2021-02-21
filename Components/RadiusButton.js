671a462fa50c40161ed4f1475cf4804b47238d78
George Kouretas <thegreekgeorge07@gmail.com>
2021-02-20 19:43:42 -0800

    Added functionality for user to pick distance

---
+++ Components/RadiusButton.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+import { View, TouchableOpacity, Text, StyleSheet } from "react-native";
+import PropTypes from 'prop-types';
+
+class RadiusButton extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            clicked: false,
+        }
+        this.select = this.select.bind(this);
+    }
+
+    select() {
+        this.setState({ clicked: !this.state.clicked });
+        this.props.changeCallback(this.props.Radius);
+    }
+
+    render() {
+        if (this.state.clicked) {
+            return (
+                <View style={styles.wrapper}>
+                    <TouchableOpacity style={styles.buttonPressed} onPress={this.select}>
+                        <Text style={styles.labelPressed}>{this.props.Radius}</Text>
+                    </TouchableOpacity>
+                </View>
+            );
+        }
+        return (
+            <View style={styles.wrapper}>
+                <TouchableOpacity style={styles.buttonUnpressed} onPress={this.select}>
+                    <Text style={styles.labelUnpressed}>{this.props.Radius}</Text>
+                </TouchableOpacity>
+            </View>
+        );
+    }
+}
+
+RadiusButton.defaultProps = {
+    isChecked: false,
+}
+
+RadiusButton.propTypes = {
+    Radius: PropTypes.string.isRequired,
+    isChecked: PropTypes.bool,
+    changeCallback: PropTypes.func.isRequired,
+}
+
+const styles = StyleSheet.create({
+    wrapper: {
+        height: '5%',
+        width: '90%',
+        color: 'white',
+        backgroundColor: 'white',
+        justifyContent: 'center',
+        borderRadius: 10,
+        marginTop: '5%',
+
+    },
+
+    buttonUnpressed: {
+        height: '100%',
+        width: '100%',
+        color: 'white',
+    },
+
+    buttonPressed: {
+        height: '100%',
+        width: '100%',
+        color: '#498B3F',
+        borderRadius: 10,
+        backgroundColor: '#498B3F',
+    },
+
+    labelUnpressed: {
+        textAlign: 'center',
+        color: '#73DC62',
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+
+    labelPressed: {
+        textAlign: 'center',
+        color: 'white',
+        fontSize: 20,
+        fontWeight: 'bold',
+    }
+});
+
+export default RadiusButton;
